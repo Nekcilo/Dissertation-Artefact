01617389fd8a3cd4440bd4195871af262675e847
Em <132229704+Nekcilo@users.noreply.github.com>
2026-03-06 15:24:21 +0000

    Arduity Intigration

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -7,17 +7,21 @@ public class Order : MonoBehaviour
 {
     [SerializeField] TMP_Text OrderLine1;
     [SerializeField] TMP_Text OrderLine2;
+    int num;
 
     // Start is called before the first frame update
     void Start()
     {
-        OrderLine1.text = "Test 1";
-        OrderLine2.text = "Test 2";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OrderLine1.text = ("Line 1: " + num++);
+            OrderLine2.text = ("Test 2: " + num++);
+        }
     }
 }
